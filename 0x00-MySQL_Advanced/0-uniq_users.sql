66474b96a65676654b52a7adf4917df299c3fd41
ElmahdiAI <mehdirajawi239@gmail.com>
2024-10-17 13:13:26 -0700

    update

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,6 @@
-create table if not EXISTS users (
-	id int not NULL AUTO_INCREMENT Primary KEY,
-	email VARCHAR(25) not null unique,
-	name varchar not null,
+-- Script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
